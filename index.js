7b6b36f3ebba9c4b13899c82b997ea7dea2a04e7
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-14 01:07:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const drivers = []
 
 drivers.push('Milo', 'Otis', 'Garfield');
 function destructivelyAppendDriver(name){
-  return drivers.push('name');
+  return drivers.push('Ralph');
 }
 function destructivelyPrependDriver(name){
-  return drivers.unshift('name');
+  return drivers.unshift('Bob');
 }
 function destructivelyRemoveLastDriver(){
   return drivers.pop();
