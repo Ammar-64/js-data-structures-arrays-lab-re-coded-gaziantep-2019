7e105c615e0fc6dfc58d1c9d4c708178d36e57ec
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-14 00:21:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your solution here!
 const drivers = []
-let destructivelyAppendDriver(name) = drivers.push('Milo', 'Otis', 'Garfield');
+let destructivelyAppendDriver = drivers.push('Milo', 'Otis', 'Garfield');
