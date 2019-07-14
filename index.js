bef0cdeb81db2232f2aeb10d5645783eb473d4d3
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-14 00:22:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
 const drivers = []
 function destructivelyAppendDriver(name){
-  return name = drivers.push('Milo', 'Otis', 'Garfield');
+  return drivers.push('Milo', 'Otis', 'Garfield');
 }
